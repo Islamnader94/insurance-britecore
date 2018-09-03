99eb450163a454a13b93fba283fb0f52c4628433
islamnader94 <eng_islamnader@hotmail.com>
2018-09-03 23:07:18 +0200

    commit

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from views import *
 app = Flask(__name__)
 
 # Set up the SQLAlchemy Database to be a local file 'risks.db'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///risks.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 db = SQLAlchemy(app)
-
+from views import *
 if __name__ == "__main__":
 
     app.run()
